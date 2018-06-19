4b4f61a6c416b156e62a5241590a6854458e4783
정현성 <jin55789@naver.com>
2018-06-19 17:45:24 +0900

    Splash Activity 추가

---
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/intro/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.tistory.jeongs0222.namdaein.ui.intro
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tistory.jeongs0222.namdaein.R
+
+class IntroActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
--- app/src/main/java/com/tistory/jeongs0222/namdaein/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/splash/SplashActivity.kt
@@ -1,12 +1,23 @@
 package com.tistory.jeongs0222.namdaein.ui.splash
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.tistory.jeongs0222.namdaein.ui.intro.IntroActivity
 
 class SplashActivity : AppCompatActivity() {
 
     override
     fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        skip()
+    }
+
+    private fun skip() {
+        val intent = Intent(this, IntroActivity::class.java)
+        startActivity(intent)
+
+        finish()
     }
 }
