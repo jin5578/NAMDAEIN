d3b2e8a054a35febabc1fac335473467c4d1c03e
정현성 <jin55789@naver.com>
2018-07-09 19:52:31 +0900

    MarketLife Item, MarketStudio Item 받아오기 연결 완료

--- app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/market/life/MarketLifeFragment.kt
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/market/life/MarketLifeFragment.kt
@@ -1,16 +1,58 @@
 package com.tistory.jeongs0222.namdaein.ui.fragment.market.life
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tistory.jeongs0222.namdaein.R
+import kotlinx.android.synthetic.main.activity_market_life_fragment.*
 
-class MarketLifeFragment : Fragment() {
+class MarketLifeFragment : Fragment(), MarketLifeContract.View {
+
+    private lateinit var mPresenter: MarketLifePresenter
+
+    private val FIRST_LOAD = 0
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_market_life_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        mPresenter = MarketLifePresenter()
+
+        mPresenter.setView(this, activity!!)
+
+        mPresenter.setUpRecyclerView()
+
+        mPresenter.setUpData(FIRST_LOAD)
+
+        mPresenter.loadMore()
+    }
+
+    override fun recyclerView(): RecyclerView {
+        return market_life_recyclerView
+    }
+
+    override fun progressBar(value: Int) {
+        when(value) {
+            0 -> market_life_progressBar.visibility = View.VISIBLE
+
+            1 -> market_life_progressBar.visibility = View.GONE
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        mPresenter.disposableClear()
     }
 }
--- app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/market/studio/MarketStudioFragment.kt
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/market/studio/MarketStudioFragment.kt
@@ -3,14 +3,51 @@ package com.tistory.jeongs0222.namdaein.ui.fragment.market.studio
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tistory.jeongs0222.namdaein.R
+import kotlinx.android.synthetic.main.activity_market_studio_fragment.*
 
-class MarketStudioFragment : Fragment() {
+class MarketStudioFragment : Fragment(), MarketStudioContract.View {
+
+    private lateinit var mPresenter: MarketStudioPresenter
+
+    private val FIRST_LOAD = 0
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_market_studio_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        mPresenter = MarketStudioPresenter()
+
+        mPresenter.setView(this, activity!!)
+
+        mPresenter.setUpRecyclerView()
+
+        mPresenter.setUpData(FIRST_LOAD)
+
+        mPresenter.loadMore()
+    }
+
+    override fun recyclerView(): RecyclerView {
+        return market_studio_recyclerView
+    }
+
+    override fun progressBar(value: Int) {
+        when(value) {
+            0 -> market_studio_progressBar.visibility = View.VISIBLE
+
+            1 -> market_studio_progressBar.visibility = View.GONE
+        }
     }
 }
