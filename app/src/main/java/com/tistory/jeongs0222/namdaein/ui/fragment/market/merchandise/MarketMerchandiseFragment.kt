a32a7a8006c4bf9e5067b59d1ae4c2af978613cb
정현성 <jin55789@naver.com>
2018-07-09 18:52:33 +0900

    MarketMerchandise Item 받아오기 연결 완료

--- app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/market/merchandise/MarketMerchandiseFragment.kt
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/market/merchandise/MarketMerchandiseFragment.kt
@@ -3,14 +3,57 @@ package com.tistory.jeongs0222.namdaein.ui.fragment.market.merchandise
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tistory.jeongs0222.namdaein.R
+import kotlinx.android.synthetic.main.activity_market_merchandise_fragment.*
 
-class MarketMerchandiseFragment : Fragment() {
+class MarketMerchandiseFragment : Fragment(), MarketMerchandiseContract.View {
+
+    private lateinit var mPresenter: MarketMerchandisePresenter
+
+    private val FIRST_LOAD = 0
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_market_merchandise_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        init()
+    }
+
+    private fun init() {
+        mPresenter = MarketMerchandisePresenter()
+
+        mPresenter.setView(this, activity!!)
+
+        mPresenter.setUpRecyclerView()
+
+        mPresenter.setUpData(FIRST_LOAD)
+
+        mPresenter.loadMore()
+    }
+
+    override fun recyclerView(): RecyclerView {
+        return market_merchandise_recyclerView
+    }
+
+    override fun progressBar(value: Int) {
+        when(value) {
+            0 -> market_merchandise_progressBar.visibility = View.VISIBLE
+
+            1 -> market_merchandise_progressBar.visibility = View.GONE
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+
+        mPresenter.disposableClear()
     }
 }
