4e0a34de9aa5f8a91f93d71f03f910c3db015100
정현성 <jin55789@naver.com>
2018-07-10 16:56:20 +0900

    BoardDetail 추가

---
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/activity/boarddetail/BoardDetailActivity.kt
@@ -0,0 +1,13 @@
+package com.tistory.jeongs0222.namdaein.ui.activity.boarddetail
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.tistory.jeongs0222.namdaein.R
+
+class BoardDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_detail)
+    }
+}
