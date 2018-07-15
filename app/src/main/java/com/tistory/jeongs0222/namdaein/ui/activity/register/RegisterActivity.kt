ae85cb7ad9a8617b3abf1b4e5701d52aacb00258
정현성 <jin55789@naver.com>
2018-07-16 01:26:54 +0900

    RegisterActivity  - Snackbar Custom

--- app/src/main/java/com/tistory/jeongs0222/namdaein/ui/activity/register/RegisterActivity.kt
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/activity/register/RegisterActivity.kt
@@ -1,11 +1,17 @@
 package com.tistory.jeongs0222.namdaein.ui.activity.register
 
+import android.graphics.Typeface
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.util.Log
+import android.view.Gravity
+import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import com.tistory.jeongs0222.namdaein.R
+import com.tistory.jeongs0222.namdaein.R.font.fonts
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity(), RegisterContract.View {
@@ -51,6 +57,16 @@ class RegisterActivity : AppCompatActivity(), RegisterContract.View {
 
     override fun snackBar(message: String) {
         val snackbar = Snackbar.make(register_entire_layout, message, Snackbar.LENGTH_SHORT)
+
+        val textView = (snackbar.view).findViewById<TextView>(android.support.design.R.id.snackbar_text)
+
+        textView.setTextSize(12F)
+
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            textView.textAlignment = View.TEXT_ALIGNMENT_CENTER
+        } else {
+            textView.gravity = Gravity.CENTER_HORIZONTAL
+        }
 
         snackbar.show()
     }
