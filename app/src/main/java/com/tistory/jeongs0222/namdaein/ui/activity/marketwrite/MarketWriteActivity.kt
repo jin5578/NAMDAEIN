ba420018def3b73bd1de236c56735889b1d6d932
정현성 <jin55789@naver.com>
2018-07-18 17:08:58 +0900

    MarketWriteActivity  - Init Design

--- app/src/main/java/com/tistory/jeongs0222/namdaein/ui/activity/marketwrite/MarketWriteActivity.kt
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/activity/marketwrite/MarketWriteActivity.kt
@@ -3,9 +3,14 @@ package com.tistory.jeongs0222.namdaein.ui.activity.marketwrite
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
 import com.tistory.jeongs0222.namdaein.R
+import kotlinx.android.synthetic.main.activity_market_write.*
 
 class MarketWriteActivity : AppCompatActivity() {
+
+    private val spinnerList = arrayOf("여성의류", "남성의류", "패션잡화", "뷰티", "도서", "티켓", "가전제품", "생활", "원룸", "기타")
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,5 +25,9 @@ class MarketWriteActivity : AppCompatActivity() {
         Log.e("sort", intent.extras.getInt("sort").toString())
 
         Log.e("order", intent.extras.getInt("order").toString())
+
+        val arrayAdapter = ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, spinnerList)
+
+        market_spinner.setAdapter(arrayAdapter)
     }
 }
