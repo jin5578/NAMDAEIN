5d567245e3927644ca9f969e45f4d252f4e33ca9
정현성 <jin55789@naver.com>
2018-08-10 17:22:35 +0900

    HolidayFragment  - 지하철 공휴일 시간 정보 추가

--- app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/subwaytimetable/holiday/HolidayFragment.kt
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/subwaytimetable/holiday/HolidayFragment.kt
@@ -3,12 +3,16 @@ package com.tistory.jeongs0222.namdaein.ui.fragment.subwaytimetable.holiday
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.tistory.jeongs0222.namdaein.R
+import kotlinx.android.synthetic.main.activity_holiday_fragment.*
 
-class HolidayFragment : Fragment() {
+class HolidayFragment : Fragment(), HolidayContract.View {
+
+    private lateinit var mPresenter: HolidayPresenter
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.activity_holiday_fragment, container, false)
@@ -21,6 +25,22 @@ class HolidayFragment : Fragment() {
     }
 
     private fun init() {
+        mPresenter = HolidayPresenter()
 
+        mPresenter.setView(this, activity!!)
+
+        mPresenter.setUpRecyclerView()
+
+        mPresenter.setUpData()
+    }
+
+    override fun recyclerView(): RecyclerView {
+        return holiday_recyclerView
+    }
+
+    override fun onDestroyView() {
+        mPresenter.disposableClear()
+
+        super.onDestroyView()
     }
 }
--- app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/subwaytimetable/holiday/HolidayPresenter.kt
+++ app/src/main/java/com/tistory/jeongs0222/namdaein/ui/fragment/subwaytimetable/holiday/HolidayPresenter.kt
@@ -1,4 +1,4 @@
-package com.tistory.jeongs0222.namdaein.ui.fragment.subwaytimetable.weekday
+package com.tistory.jeongs0222.namdaein.ui.fragment.subwaytimetable.holiday
 
 import android.content.Context
 import android.support.v7.widget.DefaultItemAnimator
@@ -10,9 +10,9 @@ import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.schedulers.Schedulers
 
 
-class WeekdayPresenter: WeekdayContract.Presenter {
+class HolidayPresenter: HolidayContract.Presenter {
 
-    private lateinit var view: WeekdayContract.View
+    private lateinit var view: HolidayContract.View
     private lateinit var context: Context
 
     private lateinit var mAdapter: SubwayItemAdapter
@@ -21,7 +21,8 @@ class WeekdayPresenter: WeekdayContract.Presenter {
 
     private val apiClient by lazy { ApiClient.create() }
 
-    override fun setView(view: WeekdayContract.View, context: Context) {
+
+    override fun setView(view: HolidayContract.View, context: Context) {
         this.view = view
         this.context = context
     }
@@ -31,6 +32,7 @@ class WeekdayPresenter: WeekdayContract.Presenter {
 
         view.recyclerView().apply {
             adapter = mAdapter
+
             layoutManager = GridLayoutManager(context, 2)
             setHasFixedSize(true)
             itemAnimator = DefaultItemAnimator()
@@ -41,7 +43,7 @@ class WeekdayPresenter: WeekdayContract.Presenter {
 
     override fun setUpData() {
         compositeDisposable
-                .add(apiClient.subwaytimetable(0)
+                .add(apiClient.subwaytimetable(1)
                         .subscribeOn(Schedulers.io())
                         .doOnNext { mAdapter.addAllItems(it.subwaytimetable) }
                         .observeOn(AndroidSchedulers.mainThread())
